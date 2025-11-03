4dd92814252e26d80864223ae9123a5a7e41b01c
montoyamoraga <montoyamoraga@gmail.com>
2025-11-03 15:12:22 -0300

    fullscreen y sin titulos y sin transform

--- mostrarPython/script.py
+++ mostrarPython/script.py
@@ -5,4 +5,4 @@ directorio_actual = os.getcwd()
 print("El directorio actual es:", directorio_actual)
 
 # abrir video con vlc
-os.system("vlc --video-filter 'transform' --transform-type '270' './../data/085.mp4'")
+os.system("vlc --fullscreen --no-sub-autodetect-file './../data/085.mp4'")
