3163bc59a0bf6acd97c3eef06a5a7e4038eb954d
montoyamoraga <montoyamoraga@gmail.com>
2025-11-03 15:17:37 -0300

    probar con mas videos

--- mostrarPython/script.py
+++ mostrarPython/script.py
@@ -4,5 +4,15 @@ import os
 directorio_actual = os.getcwd()
 print("El directorio actual es:", directorio_actual)
 
-# abrir video con vlc
-os.system("vlc --fullscreen --no-sub-autodetect-file './../data/085.mp4'")
+comandoPrefijo = "vlc --fullscreen --no-sub-autodetect-file --play-and-exit './../data/"
+comandoSufijo = ".mp4"
+
+listaVideos = ["085", "097"]
+
+for video in listaVideos:
+    comando = comandoPrefijo + video + comandoSufijo
+    os.system(comando)
+
+
+# # abrir video con vlc
+# os.system("vlc --fullscreen --no-sub-autodetect-file --play-and-exit './../data/085.mp4'")
