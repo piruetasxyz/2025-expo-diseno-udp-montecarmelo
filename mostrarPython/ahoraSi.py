ad27eb4fd4ec6f6b425abdaa6ec85d0cb2d5a548
montoyamoraga <montoyamoraga@gmail.com>
2025-11-03 17:27:28 -0300

    ahorasi

--- mostrarPython/ahoraSi.py
+++ mostrarPython/ahoraSi.py
@@ -1,41 +1,98 @@
 import vlc
+import os
 import time
 
-files = ['./../data/085.mp4', './../data/097.mp4'] 
-instances = []
-medias = []
-players = []
+# crear player
+player = vlc.Instance('--fullscreen').media_player_new()
 
-for idx, fname in enumerate(files):
-    print("Loading", fname)
-    instances.append(vlc.Instance())
-    medias.append(instances[idx].media_new(fname))
+# # crear media
+media0 = vlc.Instance().media_new('./../data/085.mp4')
+# media1 = vlc.Instance().media_new('./../data/097.mp4')
 
-    players.append(vlc.MediaPlayer())
-    players[idx].set_media(medias[idx])
-    players[idx].play()
-    players[idx].loop = True
-    players[idx].set_fullscreen(False)
+# # asignar media al player
+player.set_media(media0)
+
+# # reproducir media
+try:
+    player.play()
+except Exception as e:
+    print("Error al reproducir el video:", e)
+
+# # esperar a que termine la reproduccion
+time.sleep(10)  # ajustar el tiempo segun la duracion del video
 
 
-# copy of the players list so we don't modify during iteration
-player_count = players
-still_playing = True
-# Wait for players to start
-time.sleep(0.5)
+# while player.is_playing():
+    # nothing
+    # print("Reproduciendo...")
+    # time.sleep(1.0)
 
-while still_playing:
-    time.sleep(1)
-    for p in players:
-        if p.is_playing():
-            continue
-        else:
-            player_count.remove(p)
-            # no point iterating over players that have finished
-            players = player_count
-            print("Finished - Still playing ", str(len(player_count)))
+# player = vlc.Instance('--fullscreen').media_player_new()
+# player.set_media(media1)
 
-    if len(player_count) != 0:
-        continue
-    else:
-        still_playing = False
+# try:
+#     player.play()
+# except Exception as e:
+#     print("Error al reproducir el video:", e)
+
+
+# # obtener directorio actual
+# directorio_actual = os.getcwd()
+# print("El directorio actual es:", directorio_actual)
+
+# comandoPrefijo = "cvlc --fullscreen --no-sub-autodetect-file  --no-play-and-exit './../data/"
+# comandoSufijo = ".mp4'"
+
+# listaVideos = ["085", "097"]
+
+# for video in listaVideos:
+#     comando = comandoPrefijo + video + comandoSufijo
+#     os.system(comando)
+
+videoActual = 0
+listaVideos = ["085", "097"]
+
+
+def crearInstance():
+    return vlc.Instance()
+
+
+def crearPlayer(instancia):
+    if instancia is None:
+        instancia = crearInstance()
+    return instancia.media_player_new()
+
+
+def crearMedia(instancia, direccion):
+    return instancia.media_new(direccion)
+
+
+def reproducirVideo(player, media):
+    player.set_media(media)
+    try:
+        player.play()
+    except Exception as e:
+        print("Error al reproducir el video:", e)
+
+
+# instancia = crearInstance()
+# player = crearPlayer(instancia)
+
+# while True:
+#     print(instancia, player)
+
+#     if player is None:
+#         direccion = './../data/' + listaVideos[videoActual] + '.mp4'
+#         media = crearMedia(direccion)
+#         player = crearPlayer(instancia)
+
+#         reproducirVideo(player, media)
+#     else:
+#         if player.is_playing() is False:
+#             try: 
+#                 player.stop()
+#             except Exception as e:
+#                 print("Error al detener el video:", e)
+#             instancia = crearInstance()
+#             player = None
+#             videoActual = (videoActual + 1) % len(listaVideos)
