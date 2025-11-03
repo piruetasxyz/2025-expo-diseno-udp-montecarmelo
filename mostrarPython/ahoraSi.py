c05790d57cc824a4be4569816efa899b617c0209
montoyamoraga <montoyamoraga@gmail.com>
2025-11-03 19:01:42 -0300

    reempalzar por playAndPause

--- mostrarPython/ahoraSi.py
+++ mostrarPython/ahoraSi.py
@@ -3,7 +3,8 @@ import os
 import time
 
 # comandoPrefijo = "cvlc  --loop --fulscreen --no-sub-autodetect-file './../data/"
-comandoPrefijo = "vlc --fullscreen --no-sub-autodetect-file --no-video-title-show --play-and-exit './../data/"
+# comandoPrefijo = "vlc --fullscreen --no-sub-autodetect-file --no-video-title-show --play-and-exit './../data/"
+comandoPrefijo = "vlc --fullscreen --no-sub-autodetect-file --no-video-title-show --play-and-pause './../data/"
 
 # comandoPrefijoLoop = "vlc --fullscreen --loop 5 --no-sub-autodetect-file --no-video-title-show --play-and-exit  './../data/"
 
