cfc727fe9346f9d14a223864a86e8d915b8cf8d1
montoyamoraga <montoyamoraga@gmail.com>
2025-11-04 13:53:26 -0300

    reemplazar break por pucha

--- mostrarPython/mateo.py
+++ mostrarPython/mateo.py
@@ -16,36 +16,24 @@ cv2.namedWindow("Monitor 2 Video")
 
 # Position windows (adjust coordinates based on your monitor setup)
 # Assuming Monitor 1 is to the left of Monitor 2
-cv2.moveWindow("Monitor 1 Video", 0, 0)  # Top-left of Monitor 1
-cv2.moveWindow("Monitor 2 Video", 1920, 0) # Top-left of Monitor 2 (if Monitor 1 is 1920px wide)
+# Top-left of Monitor 1
+cv2.moveWindow("Monitor 1 Video", 0, 0)
+# Top-left of Monitor 2 (if Monitor 1 is 1920px wide)
+cv2.moveWindow("Monitor 2 Video", 1920, 0)
 
 while True:
     ret1, frame1 = cap1.read()
     ret2, frame2 = cap2.read()
 
     if not ret1 or not ret2:
-        break
+        print("pucha no hay ret1 o ret2")
 
     cv2.imshow("Monitor 1 Video", frame1)
     cv2.imshow("Monitor 2 Video", frame2)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        print("pucha se presiono q")
 
 cap1.release()
 cap2.release()
 cv2.destroyAllWindows()
-
-
-# # obtener directorio actual
-# directorio_actual = os.getcwd()
-# print("El directorio actual es:", directorio_actual)
-
-# comandoPrefijo = "cvlc --fullscreen --no-sub-autodetect-file  --no-play-and-exit './../data/"
-# comandoSufijo = ".mp4'"
-
-# listaVideos = ["085", "097"]
-
-# for video in listaVideos:
-#     comando = comandoPrefijo + video + comandoSufijo
-#     os.system(comando)
