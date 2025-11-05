d69a58255f5f6ffa79e2b1a11194edd32f084a7d
montoyamoraga <montoyamoraga@gmail.com>
2025-11-04 21:56:56 -0300

    cambiar ip

--- src/RaspiPantalla.py
+++ src/RaspiPantalla.py
@@ -1,6 +1,6 @@
 from pythonosc.dispatcher import Dispatcher
-# from pythonosc import osc_server
-from pythonosc.osc_server import BlockingOSCUDPServer
+from pythonosc import osc_server
+# from pythonosc.osc_server import BlockingOSCUDPServer
 
 
 class RaspiPantalla:
@@ -29,7 +29,8 @@ class RaspiPantalla:
         self.dispatcher = Dispatcher()
         self.dispatcher.map("/admin/bucle", self.print_handler)
         self.dispatcher.set_default_handler(self.default_handler)
-        self.server = BlockingOSCUDPServer(("127.0.0.1", 1234), self.dispatcher)
+        self.server = osc_server.ThreadingOSCUDPServer(
+            ("10.30.123.239", 1234), self.dispatcher)
         self.server.serve_forever()  # Blocks forever
 
         # self.server = osc_server.ThreadingOSCUDPServer(
