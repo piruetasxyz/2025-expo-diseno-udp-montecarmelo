c7044b81bc4b1f9a8f7b8151d1c2a434e1a9ba26
montoyamoraga <montoyamoraga@gmail.com>
2025-11-05 20:44:11 -0300

    agregar Preguntas.py

---
+++ src/Preguntas.py
@@ -0,0 +1,192 @@
+preguntas = {
+    "1":
+    {
+        "texto": (
+            "¿De qué manera el diseño contribuye a configurar nuevas "
+            "formas de vida, identidad o subjetividad humana?"
+        ),
+        "existencia": False,
+        "archivo": "01.mp4",
+    },
+    "2":
+    {
+        "texto": (
+            "¿Cómo se redefine la agencia del diseño ante las "
+            "inteligencias artificiales, saberes técnicos y conocimientos "
+            "interdisciplinares?"
+        ),
+        "existencia": False,
+        "archivo": "02.mp4",
+    },
+    "3":
+    {
+        "texto": (
+            "¿Es posible hablar hoy de un “campo del diseño” autónomo, o "
+            "toda práctica de diseño es inevitablemente interdependiente?"
+        ),
+        "existencia": False,
+        "archivo": "03.mp4",
+    },
+    "4":
+    {
+        "texto": (
+            "¿De qué manera las prácticas locales transforman nuestra "
+            "comprensión del diseño como herramienta política y cultural?"
+        ),
+        "existencia": False,
+        "archivo": "04.mp4",
+    },
+    "5":
+    {
+        "texto": (
+            "¿De qué forma las condiciones materiales, sociales y afectivas "
+            "del territorio se incorporan al proceso proyectual?"
+        ),
+        "existencia": False,
+        "archivo": "05.mp4",
+    },
+    "6":
+    {
+        "texto": (
+            "¿Cómo se construye una mirada crítica frente a los discursos "
+            "globales del diseño desde latinoamérica?"
+        ),
+        "existencia": False,
+        "archivo": "06.mp4",
+    },
+    "7":
+    {
+        "texto": (
+            "¿Puede el diseño funcionar como lenguaje común entre distintas "
+            "disciplinas? ¿Qué riesgos conlleva asumir ese rol?"
+        ),
+        "existencia": False,
+        "archivo": "07.mp4",
+    },
+    "8":
+    {
+        "texto": (
+            "¿Qué tipos de agencias se atribuyen a la materia, sistemas vivos "
+            "o inteligencias artificiales dentro del proceso de diseño? "
+        ),
+        "existencia": False,
+        "archivo": "08.mp4",
+    },
+    "9":
+    {
+        "texto": (
+            "¿Cómo cambia la figura del diseñador cuando su tarea no es "
+            "producir objetos sino mediar entre saberes, actores y lenguajes?"
+        ),
+        "existencia": False,
+        "archivo": "09.mp4",
+    },
+    "10":
+    {
+        "texto": (
+            "¿Qué futuros imagina el diseño contemporáneo y desde qué marcos "
+            "los proyecta?"
+        ),
+        "existencia": False,
+        "archivo": "10.mp4",
+    },
+    "11":
+    {
+        "texto": (
+            "¿De qué modo el diseño especulativo abre una reflexión crítica "
+            "sobre la tecnología?"
+        ),
+        "existencia": False,
+        "archivo": "11.mp4",
+    },
+    "12":
+    {
+        "texto": (
+            "¿Cómo la crisis —ecológica, tecnológica, social— redefine "
+            "nuestramanera de pensar los futuros desde el diseño?"
+        ),
+        "existencia": False,
+        "archivo": "12.mp4",
+    },
+    "13":
+    {
+        "texto": (
+            "¿De qué modo las tecnologías emergentes redefinen el diseño y "
+            "sus vínculos con lo humano y lo artificial?"
+        ),
+        "existencia": False,
+        "archivo": "13.mp4",
+    },
+    "14":
+    {
+        "texto": (
+            "¿De qué maneras el diseño puede interrogar críticamente las ?"
+            "lógicas productivas actuales"
+        ),
+        "existencia": False,
+        "archivo": "14.mp4",
+    },
+    "15":
+    {
+        "texto": (
+            "¿De qué forma el diseño hace visibles las dimensiones políticas, "
+            "éticas y simbólicas que subyacen al desarrollo tecnológico?"
+        ),
+        "existencia": False,
+        "archivo": "15.mp4",
+    },
+    "16":
+    {
+        "texto": (
+            "¿Cómo se redefinen las nociones de materialidad cuando estos son "
+            "datos, flujos, energía o información?"
+        ),
+        "existencia": False,
+        "archivo": "16.mp4",
+    },
+    "17":
+    {
+        "texto": (
+            "¿Qué vínculos se pueden establecer entre diseño, técnica "
+            "y memoria?"
+        ),
+        "existencia": False,
+        "archivo": "17.mp4",
+    },
+    "18":
+    {
+        "texto": (
+            "¿De qué maneras las prácticas situadas en América Latina "
+            "proponen otras ecologías materiales y sociales?"
+        ),
+        "existencia": False,
+        "archivo": "18.mp4",
+    },
+    "19":
+    {
+        "texto": (
+            "¿Qué rol cumplen la colaboración, la escucha y la creación en el "
+            "marco institucional de la universidad?"
+        ),
+        "existencia": False,
+        "archivo": "19.mp4",
+    },
+    "20":
+    {
+        "texto": (
+            "¿Qué nuevos roles asume el estudiante desde el aprendizaje "
+            "autónomo?"
+        ),
+        "existencia": False,
+        "archivo": "20.mp4",
+    },
+    "21":
+    {
+        "texto": (
+            "¿Qué transformaciones requiere la academia frente a los modos de "
+            "aprendizaje y prácticas del diseño contemporáneo?"
+        ),
+        "existencia": False,
+        "archivo": "21.mp4",
+    }
+}
