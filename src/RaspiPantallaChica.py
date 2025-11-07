cb2d08483bd36e638ea3bc8f226183177c3c7ce5
aarón montoya-moraga <montoyamoraga@gmail.com>
2025-11-07 20:14:04 -0300

    probar otro comando

--- src/RaspiPantallaChica.py
+++ src/RaspiPantallaChica.py
@@ -80,13 +80,15 @@ class RaspiPantallaChica(RaspiPantalla.RaspiPantalla):
                 self.comandoTotal = ""
 
                 if (self.comandoPantalla1 is not None):
-                    self.comandoTotal += self.comandoPantalla1 + "& "
-                    # os.system(self.comandoPantalla1)
-                if (self.comandoPantalla2 is not None):
-                    self.comandoTotal += self.comandoPantalla2
-                    os.system(self.comandoPantalla2)
+                    self.comandoTotal += self.comandoPantalla1 + "& " + self.comandoPantalla2
+                    os.system(self.comandoTotal)
+
+                #     # os.system(self.comandoPantalla1)
+                # if (self.comandoPantalla2 is not None):
+                #     self.comandoTotal += self.comandoPantalla2
+                #     os.system(self.comandoPantalla2)
                 # os.system(self.comandoPantalla1 + "& " + self.comandoPantalla2)
-                os.system(self.comandoTotal)
+                # os.system(self.comandoTotal)
             # else:
             #     print("pucha os.system(self.comando) era None")
             # print(f"DEFAULT {address}: {args}")
