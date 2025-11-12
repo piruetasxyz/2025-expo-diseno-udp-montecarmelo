41afc1a5ad8fee7610749093a0db15d2fa388e0c
aarón montoya-moraga <montoyamoraga@gmail.com>
2025-11-12 10:18:54 -0300

    TP-link tiene minusculas

--- simple/boot.sh
+++ simple/boot.sh
@@ -3,7 +3,7 @@
 sleep 10
 
 # CONFIGURATION
-WIFI_NAME="TP-LINK_A9A4"
+WIFI_NAME="TP-Link_A9A4"
 TARGET_DIR="$HOME/2025-expo-diseno-udp-montecarmelo/simple"
 PYTHON_SCRIPT="main.py"
 SCRIPT1="dual_vlc_generativas.sh"
