1a784f6abc44d709781b50c68a115e04668836fd
aarón montoya-moraga <montoyamoraga@gmail.com>
2025-11-12 09:53:50 -0300

    backup de ayer en simpleMartes

---
+++ simpleMartes/Datos.py
@@ -0,0 +1,16 @@
+datos = {
+    "ejes/": [],
+    "generativas/": [
+        "horizontal-1-arica/",
+        "horizontal-2-antofagasta/",
+        "horizontal-3-valpo/",
+        "horizontal-4-aysen/",
+        "horizontal-5-magallanes/",
+        "vertical-1-arica/",
+        "vertical-2-antofagasta/",
+        "vertical-3-valpo/",
+        "vertical-4-aysen/",
+        "vertical-5-magallanes/",
+    ],
+    "preguntas/": []
+}
